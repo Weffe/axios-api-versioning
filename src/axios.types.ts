648b9fbcef48d975e22ae1881999ce20e9e3b537
Weffe <rogelio_negrete@live.com>
2019-03-01 02:24:08 -0800

    fix: properly scope versioning types to axios clients with versioning
    closes #5

---
+++ src/axios.types.ts
@@ -0,0 +1,50 @@
+// Matching type definitions for axios 0.18
+
+import {
+    AxiosRequestConfig,
+    AxiosPromise,
+    AxiosInterceptorManager,
+    AxiosResponse,
+} from 'axios';
+
+/**
+ * In order to only expose the apiVersion and versioningStrategy to axios instances with
+ * versioning. We had to copy over the types and redefine them with a custom
+ * axios request config that has versioning fields defined in them.
+ */
+
+export interface AxiosRequestConfigWithVersioning extends AxiosRequestConfig {
+    apiVersion?: string;
+    versioningStrategy?: string;
+    adapter?: AxiosAdapterWithVersioning;
+}
+
+export interface AxiosAdapterWithVersioning {
+    (config: AxiosRequestConfigWithVersioning): AxiosPromise<any>;
+}
+
+export interface AxiosResponseWithVersioning<T = any> extends AxiosResponse {
+    config: AxiosRequestConfigWithVersioning;
+}
+
+export interface AxiosErrorWithVersioning extends Error {
+    config: AxiosRequestConfigWithVersioning;
+}
+
+export interface AxiosInstanceWithVersioning {
+    (config: AxiosRequestConfigWithVersioning): AxiosPromise;
+    (url: string, config?: AxiosRequestConfigWithVersioning): AxiosPromise;
+    defaults: AxiosRequestConfigWithVersioning;
+    interceptors: {
+        request: AxiosInterceptorManager<AxiosRequestConfigWithVersioning>;
+        response: AxiosInterceptorManager<AxiosResponseWithVersioning>;
+    };
+    getUri(config?: AxiosRequestConfigWithVersioning): string;
+    request<T = any, R = AxiosResponseWithVersioning<T>>(config: AxiosRequestConfigWithVersioning): Promise<R>;
+    get<T = any, R = AxiosResponseWithVersioning<T>>(url: string, config?: AxiosRequestConfigWithVersioning): Promise<R>;
+    delete<T = any, R = AxiosResponseWithVersioning<T>>(url: string, config?: AxiosRequestConfigWithVersioning): Promise<R>;
+    head<T = any, R = AxiosResponseWithVersioning<T>>(url: string, config?: AxiosRequestConfigWithVersioning): Promise<R>;
+    post<T = any, R = AxiosResponseWithVersioning<T>>(url: string, data?: any, config?: AxiosRequestConfigWithVersioning): Promise<R>;
+    put<T = any, R = AxiosResponseWithVersioning<T>>(url: string, data?: any, config?: AxiosRequestConfigWithVersioning): Promise<R>;
+    patch<T = any, R = AxiosResponseWithVersioning<T>>(url: string, data?: any, config?: AxiosRequestConfigWithVersioning): Promise<R>;
+}
--- src/types.ts
+++ src/types.ts
@@ -1,15 +1,3 @@
-import { AxiosInstance, AxiosStatic } from 'axios';
-
-type AxiosType = AxiosInstance | AxiosStatic;
-type WithVersioning = {
-    defaults: {
-        apiVersion: string;
-        versioningStrategy: VersioningStrategy;
-    }
-}
-
-export type AxiosWithVersioning = AxiosType & WithVersioning;
-
 export enum VersioningStrategy {
     QueryString = 'QUERY_STRING',
     UrlPath = 'URL_PATH',
@@ -23,6 +11,7 @@ export type MediaTypeFormatterFn = (data: {
 }) => string;
 
 export interface IVersioningConfig {
+    apiVersion: string;
     versioningStrategy: VersioningStrategy;
     mediaTypeKeyName: string;
     queryStringKeyName: string;
@@ -31,20 +20,8 @@ export interface IVersioningConfig {
 
 export interface IWithVersioningConfig
     extends PickPartial<IVersioningConfig, "mediaTypeKeyName" | "queryStringKeyName"> {
-    apiVersion?: string;
-}
-
-/**
- * modify the global axios type of AxiosRequestConfig
- * to add "apiVersion" & "versioningStrategy" to config object
- */
-declare module "axios" {
-    interface AxiosRequestConfig {
-        apiVersion?: string;
-        versioningStrategy?: string
-    }
 }
 
 // type helper
 // @see https://stackoverflow.com/a/53742583
-type PickPartial<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>> & Partial<Pick<T, K>>
+export type PickPartial<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>> & Partial<Pick<T, K>>
